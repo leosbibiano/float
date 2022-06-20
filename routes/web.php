cfcceb4122b2f4741d1f14e3ba915c90610e4430
bibiano <leonardo@floatsystem.com.br>
2022-06-20 01:12:36 -0300

    Inclusão de método para login via get

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'login', 'as' => 'login.'], function () {
+    Route::get('/sign-in', [LoginController::class, 'signIn'])->name('sign-in');
 });
