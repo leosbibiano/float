797496f3fa691630831ebbf0573bf2a35988c235
bibiano <leonardo@floatsystem.com.br>
2022-06-20 01:16:26 -0300

    route nova

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => 'login', 'as' => 'login.'], function () {
+    Route::get('/sign-in', [LoginController::class, 'signIn'])->name('sign-in');
 });
